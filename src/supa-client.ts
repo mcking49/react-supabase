f861769461db2e0f8065ce30d5811798696c0d84
Miten Chauhan <miten@chauhan.nz>
2023-02-11 10:06:25 +1100

    Generate types

--- src/supa-client.ts
+++ src/supa-client.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
+import { Database } from './database.types'
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_API_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supaClient = createClient(supabaseUrl, supabaseKey)
+export const supaClient = createClient<Database>(supabaseUrl, supabaseKey)
