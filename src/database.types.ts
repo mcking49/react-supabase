0e4a15412a0741287d467275797e54a58563f185
Miten Chauhan <miten@chauhan.nz>
2023-02-15 20:16:32 +1100

    Normalised Data Modeling

--- src/database.types.ts
+++ src/database.types.ts
@@ -28,6 +28,86 @@ export interface Database {
   }
   public: {
     Tables: {
+      post_contents: {
+        Row: {
+          content: string | null
+          created_at: string
+          id: string
+          post_id: string
+          title: string | null
+          user_id: string
+        }
+        Insert: {
+          content?: string | null
+          created_at?: string
+          id?: string
+          post_id: string
+          title?: string | null
+          user_id: string
+        }
+        Update: {
+          content?: string | null
+          created_at?: string
+          id?: string
+          post_id?: string
+          title?: string | null
+          user_id?: string
+        }
+      }
+      post_score: {
+        Row: {
+          post_id: string
+          score: number
+        }
+        Insert: {
+          post_id: string
+          score: number
+        }
+        Update: {
+          post_id?: string
+          score?: number
+        }
+      }
+      post_votes: {
+        Row: {
+          id: string
+          post_id: string
+          user_id: string
+          vote_type: string
+        }
+        Insert: {
+          id?: string
+          post_id: string
+          user_id: string
+          vote_type: string
+        }
+        Update: {
+          id?: string
+          post_id?: string
+          user_id?: string
+          vote_type?: string
+        }
+      }
+      posts: {
+        Row: {
+          created_at: string
+          id: string
+          path: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string
+          id?: string
+          path: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          path?: unknown
+          user_id?: string
+        }
+      }
       user_profiles: {
         Row: {
           user_id: string
@@ -47,7 +127,187 @@ export interface Database {
       [_ in never]: never
     }
     Functions: {
-      [_ in never]: never
+      _ltree_compress: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      _ltree_gist_options: {
+        Args: {
+          '': unknown
+        }
+        Returns: undefined
+      }
+      create_new_comment: {
+        Args: {
+          user_id: string
+          content: string
+          path: string
+        }
+        Returns: boolean
+      }
+      create_new_post: {
+        Args: {
+          userId: string
+          title: string
+          content: string
+        }
+        Returns: boolean
+      }
+      get_posts: {
+        Args: {
+          page_number: number
+        }
+        Returns: {
+          id: string
+          user_id: string
+          created_at: string
+          title: string
+          score: number
+          username: string
+        }[]
+      }
+      get_single_post_with_comments: {
+        Args: {
+          post_id: string
+        }
+        Returns: {
+          id: string
+          author_name: string
+          created_at: string
+          title: string
+          context: string
+          score: number
+          path: string
+        }[]
+      }
+      lca: {
+        Args: {
+          '': unknown[]
+        }
+        Returns: unknown
+      }
+      lquery_in: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      lquery_out: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      lquery_recv: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      lquery_send: {
+        Args: {
+          '': unknown
+        }
+        Returns: string
+      }
+      ltree_compress: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      ltree_decompress: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      ltree_gist_in: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      ltree_gist_options: {
+        Args: {
+          '': unknown
+        }
+        Returns: undefined
+      }
+      ltree_gist_out: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      ltree_in: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      ltree_out: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      ltree_recv: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      ltree_send: {
+        Args: {
+          '': unknown
+        }
+        Returns: string
+      }
+      ltree2text: {
+        Args: {
+          '': unknown
+        }
+        Returns: string
+      }
+      ltxtq_in: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      ltxtq_out: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      ltxtq_recv: {
+        Args: {
+          '': unknown
+        }
+        Returns: unknown
+      }
+      ltxtq_send: {
+        Args: {
+          '': unknown
+        }
+        Returns: string
+      }
+      nlevel: {
+        Args: {
+          '': unknown
+        }
+        Returns: number
+      }
+      text2ltree: {
+        Args: {
+          '': string
+        }
+        Returns: unknown
+      }
     }
     Enums: {
       [_ in never]: never
